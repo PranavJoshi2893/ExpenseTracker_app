1694fc7b3b9bbd9c07a33da2198e4e4895addbc3
Pranav Joshi <pranavjoshi2893@gmail.com>
2024-01-16 08:20:05 +0530

    routing converted to lazyloading routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { Routes } from '@angular/router';
-import { AuthenticationComponent } from './authentication/authentication/authentication.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { authGuard } from './authentication/shared/guard/auth.guard';
-import { TrackerComponent } from './dashboard/tracker/tracker.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
     {
-        path: "dashboard", component: DashboardComponent, canActivate: [authGuard], children: [
-            { path: "tracker", component: TrackerComponent }
+        path: "dashboard", loadComponent: () => import('./dashboard/dashboard/dashboard.component').then((c) => c.DashboardComponent), canActivate: [authGuard], children: [
+            { path: "tracker", loadComponent: () => import('./dashboard/tracker/tracker.component').then((c) => c.TrackerComponent) }
         ]
     },
-    { path: "login", component: AuthenticationComponent }
+    { path: "login", loadComponent: () => import('./authentication/authentication/authentication.component').then((c) => c.AuthenticationComponent) }
 ];
